7dd0c14686f7dcf59794c037ecac8655e772926a
Alex Ivanov <alexivanovrus@gmial.com>
2017-12-31 21:04:57 +0300

    wrote program

--- windows-design-console/Module1.vb
+++ windows-design-console/Module1.vb
@@ -7,6 +7,32 @@
         Const d As String = "#----#---#----#"
         Const x As String = "#----#--------#"
         Const y As String = "#--------#----#"
+
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine(a)
+
+        For i = 1 To 3
+            Console.WriteLine(d)
+        Next
+        Console.WriteLine(a)
+        For i = 1 To 9
+            Console.WriteLine(b)
+        Next
+        Console.WriteLine(a)
+        Console.WriteLine("")
+        Console.ForegroundColor = ConsoleColor.Yellow
+        For i = 1 To 2
+            Console.WriteLine(a)
+            For j = 1 To 3
+                Console.WriteLine(x)
+            Next
+            Console.WriteLine(a)
+            For n = 1 To 3
+                Console.WriteLine(y)
+            Next
+        Next
+        Console.WriteLine(a)
+        Console.ReadKey()
     End Sub
 
 End Module
