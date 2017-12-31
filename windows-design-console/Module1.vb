2a086278f97a7932a6d2f0e6f360021eecb6f443
Alex Ivanov <alexivanovrus@gmial.com>
2017-12-31 20:41:12 +0300

    add constants

--- windows-design-console/Module1.vb
+++ windows-design-console/Module1.vb
@@ -1,7 +1,12 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Const a As String = "###############"
+        Const b As String = "#-------------#"
+        Const c As String = "#------#------#"
+        Const d As String = "#----#---#----#"
+        Const x As String = "#----#--------#"
+        Const y As String = "#--------#----#"
     End Sub
 
 End Module
